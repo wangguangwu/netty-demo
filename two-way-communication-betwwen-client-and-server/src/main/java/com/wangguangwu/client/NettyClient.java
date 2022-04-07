ddae2a8d9762bdd93d05580cf58d9a21e635e645
汪光武 <wangguangwu@pukanghealth.com>
2022-04-07 21:07:15 +0800

    two way communication between client and server

--- two-way-communication-betwwen-client-and-server/src/main/java/com/wangguangwu/client/NettyClient.java
+++ two-way-communication-betwwen-client-and-server/src/main/java/com/wangguangwu/client/NettyClient.java
@@ -1,4 +1,4 @@
-package com.wangguangwu;
+package com.wangguangwu.client;
 
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.ChannelInitializer;
@@ -6,52 +6,41 @@ import io.netty.channel.ChannelOption;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
-import io.netty.util.AttributeKey;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.TimeUnit;
 
 /**
- * Client startup process.
- * three parameters
- * <p>
- * 1. thread model
- * 2. IO model
- * 3. business logic processing
- *
  * @author wangguangwu
  */
 @Slf4j
 public class NettyClient {
 
     private static final int MAX_RETRY = 5;
+    private static final String HOST = "127.0.0.1";
+    private static final int PORT = 8080;
 
     public static void main(String[] args) {
         NioEventLoopGroup workerGroup = new NioEventLoopGroup();
 
-        final AttributeKey<Object> clientKey = AttributeKey.newInstance("clientName");
-
         Bootstrap bootstrap = new Bootstrap();
         bootstrap
-                // 1. specify thread model
                 .group(workerGroup)
-                // 2. specify NIO type
                 .channel(NioSocketChannel.class)
-                .attr(clientKey, "nettyClient")
-                // set tcp low-level properties
                 .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
                 .option(ChannelOption.SO_KEEPALIVE, true)
                 .option(ChannelOption.TCP_NODELAY, true)
-                // 3. business logic processing
                 .handler(new ChannelInitializer<SocketChannel>() {
                     @Override
                     protected void initChannel(SocketChannel ch) {
-                        log.info("{} connecting", ch.attr(clientKey).get());
+                        // connect related logic processing chains
+                        ch.pipeline()
+                                // add a logical processor
+                                .addLast(new FirstClientHandler());
                     }
                 });
 
-        // connect server
-        connect(bootstrap, "127.0.0.1", 8080, MAX_RETRY);
+        connect(bootstrap, HOST, PORT, MAX_RETRY);
     }
 
     private static void connect(Bootstrap bootstrap, String host, int port, int retry) {
