77abbe0309aa93cd74373498a7299c100eca65de
wangguangwu <wangguangwu@pukanghealth.com>
2023-06-06 22:42:00 +0800

    A example about ByteBuf.

---
+++ chapter4/src/main/java/com/wangguangwu/bytebuf/ByteBufTest.java
@@ -0,0 +1,72 @@
+package com.wangguangwu.bytebuf;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+
+/**
+ * ByteBuf 结构
+ * <p>
+ * ByteBuf 是一个字节容器，由 3 个部分组成：
+ * <p>
+ * 1. 已经丢弃的字节，这部分数据是无效的；
+ * 2. 可读字节，这部分是 ByteBuf 的主体数据
+ * 3. 最后一部分是可写字节
+ * <p>
+ * 以上 3 段内容是由 2 个指针给划分出来的，从左到右，依次是读指针（readerIndex）、写指针（writerIndex），然后还有一个变量 capacity，表示 ByteBuf 底层内存的总容量
+ *
+ * @author wangguangwu
+ */
+public class ByteBufTest {
+
+    public static void main(String[] args) {
+        ByteBuf buffer = ByteBufAllocator.DEFAULT.buffer(9, 100);
+
+        print("allocate ByteBuf(9, 100)", buffer);
+
+        // write 方法改变写指针，写完之后写指针未到 capacity 的时候，buffer 仍然可写
+        buffer.writeBytes(new byte[]{1, 2, 3, 4});
+        print("writeBytes(1,2,3,4)", buffer);
+
+        // write 方法改变写指针，写完之后写指针未到 capacity 的时候，buffer 仍然可写, 写完 int 类型之后，写指针增加 4
+        buffer.writeInt(12);
+        print("writeInt(12)", buffer);
+
+        // write 方法改变写指针, 写完之后写指针等于 capacity 的时候，buffer 不可写
+        buffer.writeBytes(new byte[]{5});
+        print("writeBytes(5)", buffer);
+
+        // write 方法改变写指针，写的时候发现 buffer 不可写则开始扩容，扩容之后 capacity 随即改变
+        buffer.writeBytes(new byte[]{6});
+        print("writeBytes(6)", buffer);
+
+        // get 方法不改变读写指针
+        System.out.println("getByte(3) return: " + buffer.getByte(3));
+        System.out.println("getShort(3) return: " + buffer.getShort(3));
+        System.out.println("getInt(3) return: " + buffer.getInt(3));
+        System.out.println();
+        print("getByte()", buffer);
+
+        // set 方法不改变读写指针
+        buffer.setByte(buffer.readableBytes() + 1, 0);
+        print("setByte()", buffer);
+
+        // read 方法改变读指针
+        byte[] dst = new byte[buffer.readableBytes()];
+        buffer.readBytes(dst);
+        print("readBytes(" + dst.length + ")", buffer);
+    }
+
+    private static void print(String action, ByteBuf buffer) {
+        System.out.println("after ===========" + action + "============");
+        System.out.println("capacity(): " + buffer.capacity());
+        System.out.println("maxCapacity(): " + buffer.maxCapacity());
+        System.out.println("readerIndex(): " + buffer.readerIndex());
+        System.out.println("readableBytes(): " + buffer.readableBytes());
+        System.out.println("isReadable(): " + buffer.isReadable());
+        System.out.println("writerIndex(): " + buffer.writerIndex());
+        System.out.println("writableBytes(): " + buffer.writableBytes());
+        System.out.println("isWritable(): " + buffer.isWritable());
+        System.out.println("maxWritableBytes(): " + buffer.maxWritableBytes());
+        System.out.println();
+    }
+}
